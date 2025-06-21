9b9be220d63924d4687d9d4a064dcf2e95043042
MEURSAULT A <Traore Souroge Oumar>
2025-06-21 23:48:55 +0100

    Force redeploy on Render

--- src/utils/avatarUrl.js
+++ src/utils/avatarUrl.js
@@ -3,12 +3,10 @@ import API_URL from "../config";
 export function getAvatarUrl(avatar) {
   if (!avatar) return "/default-avatar.png";
   if (avatar.startsWith("http")) return avatar;
-
   const filename = avatar
     .replace("media/", "")
     .replace("avatars/", "")
     .replace("user/avatar/", "")
     .replace(/^\/+/, "");
-
   return `${API_URL}/user/avatar/${filename}`;
 }
