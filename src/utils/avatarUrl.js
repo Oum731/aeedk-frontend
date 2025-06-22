d0ced1c1fe670a439a1b862c7c8b1767deca7181
MEURSAULT A <Traore Souroge Oumar>
2025-06-22 14:59:46 +0100

    Force redeploy on Render

--- src/utils/avatarUrl.js
+++ src/utils/avatarUrl.js
@@ -3,8 +3,7 @@ import API_URL from "../config";
 export function getAvatarUrl(avatar) {
   if (!avatar) return "/default-avatar.png";
   if (avatar.startsWith("http")) return avatar;
-  return `https://aeedk-backend.onrender.com/api/user/avatar/${avatar.replace(
-    /^avatars[\\/]/,
-    ""
-  )}`;
+  let filename = avatar.replace(/^media[\\/]/, "");
+  filename = filename.replace(/^avatars[\\/]/, "");
+  return `https://aeedk-backend.onrender.com/api/user/avatar/${filename}`;
 }
