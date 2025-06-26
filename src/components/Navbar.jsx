a1260583ac0d8a37cbc04fdf6d1e9894e7231255
MEURSAULT A <Traore Souroge Oumar>
2025-06-26 17:54:25 +0100

    Force redeploy on Render

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -157,7 +157,7 @@ export default function Navbar() {
         {user ? (
           <button
             onClick={(e) => handleLinkClick("/profile", e)}
-            className="flex items-center gap-3 px-3 py-2 rounded-md hover:bg-blue-600 hover:text-white
+            className="flex items-center gap-3  py-2 rounded-md hover:bg-blue-600 hover:text-white
               focus:outline-none focus:ring-2 focus:ring-blue-600 justify-center w-full"
             title={displayName}
             tabIndex={0}
